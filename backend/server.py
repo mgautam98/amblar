74bc9793bcddb29cbd0464cee0fae6bb3bab9826
Gautam Mishra <mishragautam96@gmail.com>
2020-04-11 19:19:51 +0530

    segg changes

--- backend/server.py
+++ backend/server.py
@@ -1,33 +1,6 @@
 import os
 from flask import Flask
-import mysql.connector
-
-
-class DBManager:
-    def __init__(self, database='example', host="db", user="root", password_file=None):
-        pf = open(password_file, 'r')
-        self.connection = mysql.connector.connect(
-            user=user, 
-            password=pf.read(),
-            host=host, # name of the mysql service as set in the docker-compose file
-            database=database,
-            auth_plugin='mysql_native_password'
-        )
-        pf.close()
-        self.cursor = self.connection.cursor()
-    
-    def populate_db(self):
-        self.cursor.execute('DROP TABLE IF EXISTS blog')
-        self.cursor.execute('CREATE TABLE blog (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255))')
-        self.cursor.executemany('INSERT INTO blog (id, title) VALUES (%s, %s);', [(i, 'Blog post #%d'% i) for i in range (1,5)])
-        self.connection.commit()
-    
-    def query_titles(self):
-        self.cursor.execute('SELECT title FROM blog')
-        rec = []
-        for c in self.cursor:
-            rec.append(c[0])
-        return rec
+from db_manager import DBManager
 
 
 server = Flask(__name__)
@@ -43,7 +16,7 @@ def listBlog():
 
     response = ''
     for c in rec:
-        response = response  + '<div>   Hello  ' + c + '</div>'
+        response = response  + '<div>   Hello ' + c + '</div>'
     return response
 
 
